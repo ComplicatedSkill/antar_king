282276881912e0e65a69dd4339101a1ce1c6f20d
Jangouji99 <jangouji99@gmail.com>
2023-09-19 10:31:52 +0700

    first commit

---
+++ app/src/main/java/com/atarking_project/screens/ScreenSearchProductActivity.java
@@ -0,0 +1,16 @@
+package com.atarking_project.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.atarking_project.R;
+
+public class ScreenSearchProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_search_product);
+    }
+}
